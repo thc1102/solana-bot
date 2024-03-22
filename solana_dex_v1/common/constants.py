606b1338c0607f59244496ee5d227d703b245bac
tianhaochen <tianhaochen@bmb1688.com>
2024-03-22 13:41:53 +0800

    搞不动了先在这里吧

--- solana_dex_v1/common/constants.py
+++ solana_dex_v1/common/constants.py
@@ -9,3 +9,5 @@ RAYDIUM_LIQUIDITY_POOL_V4 = Pubkey.from_string("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9
 RAY_AUTHORITY_V4 = Pubkey.from_string("5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1")
 
 OPENBOOK_MARKET = Pubkey.from_string("srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX")
+
+LAMPORTS_PER_SOL = 1000000000
--- solana_dex_v1/raydium/swap_core.py
+++ solana_dex_v1/raydium/swap_core.py
@@ -1,6 +1,23 @@
+from solders.keypair import Keypair
+from solders.pubkey import Pubkey
+from solders.token.associated import get_associated_token_address
+
+from solana_dex_v1.common.constants import LAMPORTS_PER_SOL
+from solana_dex_v1.raydium.swap_util import make_swap_instruction
+
+
 class SwapCore:
-    async def buy(self):
-        pass
+
+    async def buy(self, token_to_swap_buy, amount, payer: Keypair, pool_info):
+        # 获取要购买代币的Pukey
+        mint = Pubkey.from_string(token_to_swap_buy)
+        # 计算付款金额
+        amount_in = int(amount * LAMPORTS_PER_SOL)
+        # create account with seed
+        # source = await self.append_create_account_with_seed(lamports)
+        # swap
+        # dest = get_associated_token_address(payer.pubkey(), mint)
+        # instructions_swap = make_swap_instruction(amount_in, source, dest, payer, pool_info)
 
     async def sell(self):
         pass
