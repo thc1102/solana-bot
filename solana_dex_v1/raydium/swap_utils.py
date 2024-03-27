b9b38d7a52f754ccb42463d66ae8d0e7c64a5e22
tianhaochen <tianhaochen@bmb1688.com>
2024-03-28 02:26:49 +0800

    更改代码

--- solana_dex_v1/raydium/swap_utils.py
+++ solana_dex_v1/raydium/swap_utils.py
@@ -1,7 +1,6 @@
-from typing import Union
-
 import solders.system_program as sp
 import spl.token.instructions as spl_token
+from solana.rpc.async_api import AsyncClient
 from solders.compute_budget import set_compute_unit_price, set_compute_unit_limit
 from solders.instruction import AccountMeta, Instruction
 from solders.keypair import Keypair
@@ -14,7 +13,7 @@ from spl.token.async_client import AsyncToken
 from spl.token.constants import TOKEN_PROGRAM_ID
 from spl.token.instructions import close_account, CloseAccountParams, create_associated_token_account
 
-from solana_dex_v1.common.constants import OPENBOOK_MARKET, RAYDIUM_LIQUIDITY_POOL_V4
+from solana_dex_v1.common.constants import OPENBOOK_MARKET, RAYDIUM_LIQUIDITY_POOL_V4, SOL_MINT_ADDRESS
 from solana_dex_v1.layout.raydium import ROUTE_DATA_LAYOUT
 from solana_dex_v1.model.pool import PoolInfo
 
@@ -28,20 +27,20 @@ def make_swap_instruction(
 ) -> Instruction:
     keys = [
         AccountMeta(pubkey=TOKEN_PROGRAM_ID, is_signer=False, is_writable=False),
-        AccountMeta(pubkey=pool_info.id, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.authority, is_signer=False, is_writable=False),
-        AccountMeta(pubkey=pool_info.openOrders, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.targetOrders, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.baseVault, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.quoteVault, is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.id), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.authority), is_signer=False, is_writable=False),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.openOrders), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.targetOrders), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.baseVault), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.quoteVault), is_signer=False, is_writable=True),
         AccountMeta(pubkey=OPENBOOK_MARKET, is_signer=False, is_writable=False),
-        AccountMeta(pubkey=pool_info.marketId, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.marketBids, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.marketAsks, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.marketEventQueue, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.marketBaseVault, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.marketQuoteVault, is_signer=False, is_writable=True),
-        AccountMeta(pubkey=pool_info.marketAuthority, is_signer=False, is_writable=False),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.marketId), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.marketBids), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.marketAsks), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.marketEventQueue), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.marketBaseVault), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.marketQuoteVault), is_signer=False, is_writable=True),
+        AccountMeta(pubkey=Pubkey.from_string(pool_info.marketAuthority), is_signer=False, is_writable=False),
         AccountMeta(pubkey=token_account_in, is_signer=False, is_writable=True),
         AccountMeta(pubkey=token_account_out, is_signer=False, is_writable=True),
         AccountMeta(pubkey=owner, is_signer=True, is_writable=False)
@@ -55,3 +54,201 @@ def make_swap_instruction(
         )
     )
     return Instruction(RAYDIUM_LIQUIDITY_POOL_V4, data, keys)
+
+
+class SwapTransactionBuilder:
+    def __init__(
+            self,
+            client: AsyncClient,
+            pool: PoolInfo,
+            payer: Keypair,
+            unit_price: int = 25000,
+            unit_budget: int = 600000,
+    ):
+        self.client = client
+        self.pool = pool
+        self.payer = payer
+        # token address
+        self.baseMint = Pubkey.from_string(pool.baseMint)
+        self.TOKEN_PROGRAM_ID = TOKEN_PROGRAM_ID
+        # quote address (supposed to be SOL)
+        self.quoteMint = Pubkey.from_string(pool.quoteMint)
+        # budget
+        self.unit_price = unit_price
+        self.unit_budget = unit_budget
+        # initialize instructions
+        self.instructions = []
+        self.new_keypair = None
+
+    async def compile_versioned_transaction(self):
+        try:
+            recent_blockhash = self.client.blockhash_cache.get()
+        except:
+            recent_blockhash = (await self.client.get_latest_blockhash()).value
+        compiled_message = MessageV0.try_compile(
+            self.payer.pubkey(),
+            self.instructions,
+            [],  # lookup tables
+            recent_blockhash.blockhash,
+        )
+        keypairs = [self.payer]
+        if self.new_keypair is not None:
+            keypairs.append(self.new_keypair)
+        return VersionedTransaction(compiled_message, keypairs)
+
+    def append_swap(
+            self, amount_in: int, source: Pubkey, dest: Pubkey
+    ):
+        self.instructions.append(
+            make_swap_instruction(
+                amount_in,
+                source,
+                dest,
+                self.payer.pubkey(),
+                self.pool,
+            )
+        )
+
+    async def wsol_append_buy(self, amount_in: int, check_associated_token_account_exists=True):
+        # 前提必须存在wsol代币地址
+        self.append_set_compute_budget(self.unit_price, self.unit_budget)
+        source = get_associated_token_address(self.payer.pubkey(), SOL_MINT_ADDRESS)
+        dest = get_associated_token_address(self.payer.pubkey(), self.baseMint)
+        if check_associated_token_account_exists:
+            await self.append_if_not_exists_create_associated_token_account(self.baseMint)
+        self.append_swap(amount_in, source, dest)
+
+    async def sol_append_buy(self, amount_in: int, check_associated_token_account_exists=True, ):
+        pay_for_rent = await AsyncToken.get_min_balance_rent_for_exempt_for_account(self.client)
+        self.append_set_compute_budget(self.unit_price, self.unit_budget)
+        source = self.append_create_account(pay_for_rent, amount_in)
+        self.append_initialize_account(source.pubkey())
+        if check_associated_token_account_exists:
+            await self.append_if_not_exists_create_associated_token_account(self.baseMint)
+        dest = get_associated_token_address(self.payer.pubkey(), self.baseMint)
+        self.append_swap(amount_in, source.pubkey(), dest)
+        self.append_close_account(source.pubkey())
+
+    async def wsol_append_sell(self, amount_in: int):
+        # 前提必须存在wsol代币地址
+        self.append_set_compute_budget(self.unit_price, self.unit_budget)
+        source = get_associated_token_address(self.payer.pubkey(), self.baseMint)
+        dest = get_associated_token_address(self.payer.pubkey(), SOL_MINT_ADDRESS)
+        self.append_swap(amount_in, source, dest)
+
+    async def sol_append_sell(self, amount_in: int):
+        pay_for_rent = await AsyncToken.get_min_balance_rent_for_exempt_for_account(self.client)
+        self.append_set_compute_budget(self.unit_price, self.unit_budget)
+        source = get_associated_token_address(self.payer.pubkey(), self.baseMint)
+        dest = self.append_create_account(pay_for_rent, 0)
+        self.append_initialize_account(dest.pubkey())
+        self.append_swap(amount_in, source, dest.pubkey())
+        self.append_close_account(dest.pubkey())
+
+    def append_set_compute_budget(self, unit_price: int, unit_limit: int):
+        self.instructions.append(set_compute_unit_price(unit_price))
+        self.instructions.append(set_compute_unit_limit(unit_limit))
+
+    def append_close_account(self, account: Pubkey):
+        self.instructions.append(
+            close_account(
+                CloseAccountParams(
+                    account=account,
+                    dest=self.payer.pubkey(),
+                    owner=self.payer.pubkey(),
+                    program_id=self.TOKEN_PROGRAM_ID,
+                )
+            )
+        )
+
+    def append_create_associated_token_account(self, mint: Pubkey):
+        self.instructions.append(
+            create_associated_token_account(
+                self.payer.pubkey(), self.payer.pubkey(), mint
+            )
+        )
+
+    def append_create_account(self, lamports: int, amount: int == 0):
+        new_keypair = Keypair()
+        self.instructions.append(
+            sp.create_account(
+                sp.CreateAccountParams(
+                    from_pubkey=self.payer.pubkey(),
+                    to_pubkey=new_keypair.pubkey(),
+                    lamports=lamports,
+                    space=ACCOUNT_LAYOUT.sizeof(),
+                    owner=self.TOKEN_PROGRAM_ID,
+                )
+            )
+        )
+        if amount != 0:
+            self.instructions.append(
+                sp.transfer(
+                    sp.TransferParams(
+                        from_pubkey=self.payer.pubkey(),
+                        to_pubkey=new_keypair.pubkey(),
+                        lamports=amount,
+                    )
+                )
+            )
+        self.new_keypair = new_keypair
+        return new_keypair
+
+    def append_initialize_account(self, source: Pubkey):
+        # initialize account
+        self.instructions.append(
+            spl_token.initialize_account(
+                spl_token.InitializeAccountParams(
+                    account=source,
+                    mint=SOL_MINT_ADDRESS,
+                    owner=self.payer.pubkey(),
+                    program_id=self.TOKEN_PROGRAM_ID,
+                )
+            )
+        )
+
+    async def append_if_not_exists_create_associated_token_account(self, mint: Pubkey):
+        # arr = (await self.client.get_token_accounts_by_owner(self.payer.pubkey(), TokenAccountOpts(mint))).value
+        #
+        # if len(arr) > 0:
+        #     return
+
+        self.instructions.append(
+            create_associated_token_account(
+                self.payer.pubkey(), self.payer.pubkey(), mint
+            )
+        )
+
+
+class AccountTransactionBuilder:
+    def __init__(self, client: AsyncClient, payer: Keypair):
+        self.client = client
+        self.payer = payer
+        self.TOKEN_PROGRAM_ID = TOKEN_PROGRAM_ID
+        self.instructions = []
+
+    def append_close_account(self, account: Pubkey):
+        self.instructions.append(
+            close_account(
+                CloseAccountParams(
+                    account=account,
+                    dest=self.payer.pubkey(),
+                    owner=self.payer.pubkey(),
+                    program_id=self.TOKEN_PROGRAM_ID,
+                )
+            )
+        )
+
+    async def compile_versioned_transaction(self):
+        try:
+            recent_blockhash = self.client.blockhash_cache.get()
+        except:
+            recent_blockhash = (await self.client.get_latest_blockhash()).value
+        compiled_message = MessageV0.try_compile(
+            self.payer.pubkey(),
+            self.instructions,
+            [],  # lookup tables
+            recent_blockhash.blockhash,
+        )
+        keypairs = [self.payer]
+        return VersionedTransaction(compiled_message, keypairs)
---
+++ solana_dex_v1/solana/wallet.py
@@ -0,0 +1,77 @@
+import asyncio
+import json
+import time
+from typing import Optional
+
+from loguru import logger
+from solana.rpc.commitment import Commitment, Finalized
+from solana.rpc.types import TokenAccountOpts
+from solders.keypair import Keypair
+
+from solana_dex.common.constants import TOKEN_PROGRAM_ID
+from solana_dex.solana.solana_client import SolanaRPCClient
+
+lock = asyncio.Lock()
+
+
+class TokenAccountData:
+    def __init__(self, address, mint, amount, ui_amount, decimals):
+        # 钱包中的代币地址
+        self.address = address
+        # 代币的Mint
+        self.mint = mint
+        # 代币余额
+        self.amount = amount
+        # 带小数点的余额
+        self.uiAmount = ui_amount
+        # 小数点转换位数
+        self.decimals = decimals
+
+
+class Wallet:
+    def __init__(self, keypair: str):
+        self.keypair = Keypair.from_base58_string(keypair)
+        self.pubkey = self.keypair.pubkey()
+        self.token_data = {}
+
+    async def get_sol_balance(self):
+        balance_resp = await SolanaRPCClient.get_balance(self.pubkey)
+        return balance_resp.value / 10 ** 9
+
+    async def update_token_accounts(self, commitment: Optional[Commitment] = Finalized):
+        try:
+            balance = await SolanaRPCClient.get_token_accounts_by_owner_json_parsed(self.pubkey,
+                                                                                    TokenAccountOpts(
+                                                                                        program_id=TOKEN_PROGRAM_ID),
+                                                                                    commitment)
+            token_data = {}
+            for item in balance.value:
+                address = item.pubkey
+                mint = item.account.data.parsed["info"]["mint"]
+                amount = item.account.data.parsed["info"]["tokenAmount"]["amount"]
+                ui_amount = item.account.data.parsed["info"]["tokenAmount"]["uiAmount"]
+                decimals = item.account.data.parsed["info"]["tokenAmount"]["decimals"]
+                token_data[mint] = TokenAccountData(address, mint, amount, ui_amount, decimals)
+            # 加锁保证并发不会影响
+            async with lock:
+                self.token_data = token_data
+        except:
+            return False
+        return True
+
+    def get_token_accounts(self, mint):
+        if not isinstance(mint, str):
+            mint = str(mint)
+        return self.token_data.get(mint, None)
+
+    def check_token_accounts(self, mint):
+        if not isinstance(mint, str):
+            mint = str(mint)
+        return mint in self.token_data
+
+    def get_no_balance_account(self):
+        account_list = []
+        for _, data in self.token_data.items():
+            if int(data.amount) == 0:
+                account_list.append(data.address)
+        return account_list
