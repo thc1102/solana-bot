6f1f3d144d28238859917e2bf29681435b8b48bc
tianhaochen <tianhaochen@bmb1688.com>
2024-04-02 01:44:47 +0800

    提交

--- solana_dex/websocket/liquidity1.py
+++ solana_dex/websocket/liquidity1.py
@@ -3,22 +3,24 @@ import asyncio
 import websockets
 from asyncstdlib import enumerate
 from loguru import logger
+from solana.rpc.async_api import AsyncClient
 from solana.rpc.commitment import Processed, Confirmed
-from solana.rpc.types import DataSliceOpts, MemcmpOpts
 from solana.rpc.websocket_api import connect
 from solders.pubkey import Pubkey
 from solders.rpc.config import RpcTransactionLogsFilterMentions
 
 # from solana_dex.tasks_processor import TasksProcessor
 
-from solana_dex.common.constants import RAYDIUM_LIQUIDITY_POOL_V4
-from solana_dex.layout.raydium import LIQUIDITY_STATE_LAYOUT_V4
+solana_client = AsyncClient(
+    "https://aged-few-sailboat.solana-mainnet.quiknode.pro/a59a384a0e707c877100881079c24ebfee00eb1b/")
 
 
 async def parse_liqudity_data(data):
     try:
         if not data.result.value.err:
-            print(data.result.value)
+            logger.info(data.result.value)
+            logger.info((await solana_client.get_transaction(data.result.value.signature, commitment=Confirmed,
+                                                             max_supported_transaction_version=2)))
         # await TasksProcessor.liqudity_tasks(str(data.result.value.pubkey), liqudity_info)
     except Exception as e:
         logger.error(e)
@@ -31,7 +33,8 @@ async def run():
             async with connect(
                     "wss://aged-few-sailboat.solana-mainnet.quiknode.pro/a59a384a0e707c877100881079c24ebfee00eb1b/") as wss:
                 await wss.logs_subscribe(
-                    RpcTransactionLogsFilterMentions(Pubkey.from_string("7YttLkHDoNj9wyDur5pM1ejNaAvT9X4eqaYcHQqtj2G5")), Processed
+                    RpcTransactionLogsFilterMentions(
+                        Pubkey.from_string("7YttLkHDoNj9wyDur5pM1ejNaAvT9X4eqaYcHQqtj2G5")), Processed
                     # data_slice=DataSliceOpts(length=752, offset=0),
                     # filters=[
                     #     MemcmpOpts(offset=432, bytes="So11111111111111111111111111111111111111112"),
